dd60b83211aba425e59c1059dd75d167de5e7190
IM BIG BACON........2 <coderlad@gmail.com>
2014-06-24 13:56:27 -0700

    Create calculater.py

---
+++ calculater.py
@@ -0,0 +1,13 @@
+a = input("type 1 number")
+b = input("type 1 number")
+c = "your numbers subtracted, added, multiplyed,"
+d = "and divided(enter in this exact order, they will "
+e = "be printed by each other in this exact order.) "
+f = "are: "
+print c 
+print d
+print e
+print int(a) - int(b)
+print int(a) + int(b)
+print int(a) * int(b)
+print int(a) / int(b)
