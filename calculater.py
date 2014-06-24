0a164c7801512f2d4015e12107fd3f38046973b8
IM BIG BACON........2 <coderlad@gmail.com>
2014-06-24 15:47:16 -0700

    pythonroom update

--- calculater.py
+++ calculater.py
@@ -1,5 +1,5 @@
 a = input("type 1 number")
-b = input("type 1 number")
+b = input("type another number")
 c = "your numbers subtracted, added, multiplyed,"
 d = "and divided(enter in this exact order, they will "
 e = "be printed by each other in this exact order.) "
