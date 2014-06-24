961b17db490b3cf2144220c4468d2be8d0dd8212
IM BIG BACON........2 <coderlad@gmail.com>
2014-06-24 14:23:33 -0700

    pythonroom update

--- if-statements.py
+++ if-statements.py
@@ -1,1 +1,20 @@
 # author: Big-Bacon2
+if country == "US":
+    if total <= "50":
+        print "Shipping Costs $6.00"
+    elif total <= "100":
+            print "Shipping Costs $9.00"
+    elif total <= "150":
+            print "Shipping Costs $12.00"
+    else:
+        print "FREE"
+
+if country == "Canada":
+    if total <= "50":
+        print "Shipping Costs $8.00"
+    elif total <= "100":
+        print "Shipping Costs $12.00"
+    elif total <= "150":
+        print "Shipping Costs $15.00"
+    else:
+        print "FREE"
