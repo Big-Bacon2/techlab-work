f1759d8d9815256dd94d54a7ada1b05229be6227
IM BIG BACON........2 <coderlad@gmail.com>
2014-06-25 14:10:28 -0700

    initial commit

---
+++ moldycheese.py
@@ -0,0 +1,23 @@
+]grade = int(input("Enter Percentage."))
+b = 0
+if grade > 93:
+	b = "A"
+	print b 
+else:
+	#if grade > 79 and 79 <=91:
+	if grade > 79 and grade <= 91
+		b = "B"
+		print b 
+	else:
+		if grade > 74 <= 82:
+			b = "C"
+			print b 
+		else:
+			if grade > 65 <= 76:
+				b = "D"
+				print b 
+			else:
+				if grade < 66 :
+					b = "F"
+					print b
+					
