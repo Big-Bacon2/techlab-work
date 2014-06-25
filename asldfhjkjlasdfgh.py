e996d2519f52fe6e3ddf45035c538a17d9c75d85
IM BIG BACON........2 <coderlad@gmail.com>
2014-06-25 15:50:16 -0700

    pythonroom update

--- asldfhjkjlasdfgh.py
+++ asldfhjkjlasdfgh.py
@@ -1,1 +1,14 @@
 # author: Big-Bacon2
+ grade = float(input("Type a percent:"))
+if grade >= 90:
+	print "You earned an A on your recent test!"
+elif grade <= 89 and grade >= 80:
+	print "You earned an B on your recent test!"
+elif grade <= 79 and grade >= 70:
+	print "You earned an C on your recent test!"
+elif grade <= 69 and grade >= 51:
+	print "You earned an D on your recent test!"
+elif grade <= 50 and grade > 1:
+	print "You earned an F on your recent test!"
+elif grade <=1:
+	print "You earned a grade unknown to school teachers!"
