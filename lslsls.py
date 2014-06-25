43b22f68cb187a755b2f3bbcefb01e183a1aa88c
IM BIG BACON........2 <coderlad@gmail.com>
2014-06-25 13:07:48 -0700

    initial commit

---
+++ lslsls.py
@@ -0,0 +1,12 @@
+a = input("type 1 number")
+b = input("type another number")
+c = "your numbers subtracted, added, multiplyed,"
+d = "and divided are: "
+print c 
+print d
+print e
+print f
+print int(a) - int(b)
+print int(a) + int(b)
+print int(a) * int(b)
+print int(a) / int(b)
