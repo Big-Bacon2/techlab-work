35c3ab2ee6c23bc7244a4fcee4d65d333843d1c7
IM BIG BACON........2 <coderlad@gmail.com>
2014-06-25 15:39:12 -0700

    pythonroom update

--- h.py
+++ h.py
@@ -1,7 +1,7 @@
 word = input("type a word.")
 total = 0
 for letter in word:
-	if letter in ['q','w','r','t','p','s','d','f','g','h','j','k''l','z','x','c','v','b','n','m']:
+	if not (letter in ['a','e','i','o','u']):
 		total = total + 1
 
 print total
