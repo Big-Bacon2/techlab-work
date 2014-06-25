564e35acad987f1caa66ca36bae9e3148c287893
IM BIG BACON........2 <coderlad@gmail.com>
2014-06-25 14:59:20 -0700

    pythonroom update

--- moldycheese.py
+++ moldycheese.py
@@ -1,23 +1,7 @@
 word = input("type a word.")
-b = word[0]
-c = word[1]
-d = word[2] 
-e = word[3]
-f = word[4]
-g = word[5]
-h = word[6]
-if c in ["a","e","i","u"]:
-	a = 0
-	a + 1
-	if b in ["a","e","i","u"]:
-	    a + 1
-		if d in ["a","e","i","u"]:
-			a + 1
-			if e in ["a","e","i","u"]:
-				a + 1
-				if f in ["a","e","i","u"]:
-					a + 1
-					if g in ["a","e","i","u"]:
-	                    a + 1
-						if h in ["a","e","i","u"]:
-							a + 1
+total = 0
+for letter in word:
+	if letter in ['a','e','i','o','u']:
+		total = total + 1
+
+print total
