103f9ed5e1f47eb8300f8ed25ffe338ff91dba52
IM BIG BACON........2 <coderlad@gmail.com>
2014-06-26 15:19:31 -0700

    pythonroom update

--- fghjkfglhkjlkdfghjkldfghj;lkdfghjldf;kghj.py
+++ fghjkfglhkjlkdfghjkldfghj;lkdfghjldf;kghj.py
@@ -1,6 +1,8 @@
 # author: Big-Bacon2
 import turtle
 t = turtle.Turtle()
-for x in range(0, 9999)
-t.forward(1)
-t.left(1)
+t.speed(0)
+t.color("red")
+for x in range(0, 9999):
+	t.forward(1)
+	t.left(1)
