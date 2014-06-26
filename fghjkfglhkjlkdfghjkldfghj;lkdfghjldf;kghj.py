bfa79ee32016ff85180ec1b00dd71aa36bcc03ee
IM BIG BACON........2 <coderlad@gmail.com>
2014-06-26 15:58:28 -0700

    pythonroom update

--- fghjkfglhkjlkdfghjkldfghj;lkdfghjldf;kghj.py
+++ fghjkfglhkjlkdfghjkldfghj;lkdfghjldf;kghj.py
@@ -3,21 +3,79 @@ import turtle
 t = turtle.Turtle()
 t.speed(0)
 t.color("red")
-for x in range(0, 9999):
+for x in range(0, 999):
+	t.color("red")
+	t.forward(10)
+	t.left(90)
+	t.left(10)
 	t.color("orange")
-	t.forward(1)
+	t.forward(10)
 	
-	t.left(1)
+	t.left(10)
 	t.color("yellow")
-	t.forward(1)
+	t.forward(10)
 	
-	t.left(1)
+	t.left(10)
 	t.color("green")
-	t.forward(1)
+	t.forward(10)
 	
-	t.left(1)
+	t.left(10)
 	t.color("blue")
-	t.forward(1)
+	t.forward(10)
 	
-	t.left(1)
+	t.left(10)
 	t.color("purple")
+	
+	u = turtle.Turtle()
+u.speed(0)
+u.color("red")
+for x in range(0, 999):
+	u.color("red")
+	u.forward(9)
+	u.left(90)
+	u.left(9)
+	u.color("orange")
+	u.forward(9)
+	
+	u.left(9)
+	u.color("yellow")
+	u.forward(9)
+	
+	u.left(9)
+	u.color("green")
+	u.forward(9)
+	
+	u.left(9)
+	u.color("blue")
+	u.forward(9)
+	
+	u.left(9)
+	u.color("purple")
+	
+	r = turtle.Turtle()
+r.speed(0)
+r.color("red")
+for x in range(0, 999):
+	r.color("red")
+	r.left(90)
+	r.left(9)
+	r.forward(8)
+	
+	r.left(8)
+	r.color("orange")
+	r.forward(8)
+	
+	r.left(8)
+	r.color("yellow")
+	r.forward(8)
+	
+	r.left(8)
+	r.color("green")
+	r.forward(8)
+	
+	r.left(8)
+	r.color("blue")
+	r.forward(8)
+	
+	r.left(8)
+	r.color("purple")
