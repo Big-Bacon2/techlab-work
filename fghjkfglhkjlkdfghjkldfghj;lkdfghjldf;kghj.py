d0dbde6a8acfcce6a200bfcceea32b5b41113ac9
IM BIG BACON........2 <coderlad@gmail.com>
2014-06-26 15:30:16 -0700

    pythonroom update

--- fghjkfglhkjlkdfghjkldfghj;lkdfghjldf;kghj.py
+++ fghjkfglhkjlkdfghjkldfghj;lkdfghjldf;kghj.py
@@ -4,5 +4,20 @@ t = turtle.Turtle()
 t.speed(0)
 t.color("red")
 for x in range(0, 9999):
+	t.color("orange")
 	t.forward(1)
+	
 	t.left(1)
+	t.color("yellow")
+	t.forward(1)
+	
+	t.left(1)
+	t.color("green")
+	t.forward(1)
+	
+	t.left(1)
+	t.color("blue")
+	t.forward(1)
+	
+	t.left(1)
+	t.color("purple")
