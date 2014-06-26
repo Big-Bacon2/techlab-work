e0b2776fa50605ec5f3b9f9ccc3e7699449f20ed
IM BIG BACON........2 <coderlad@gmail.com>
2014-06-26 13:50:31 -0700

    initial commit

---
+++ flowsdfg.py
@@ -0,0 +1,10 @@
+a = input("type 1 number")
+b = input("type another number")
+c = "your numbers subtracted, added, multiplyed,"
+d = "and divided are: "
+print c 
+print d
+print int(a) - int(b)
+print int(a) + int(b)
+print int(a) * int(b)
+print int(a) / int(b)
